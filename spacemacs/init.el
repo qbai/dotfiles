f9ac4f08af3b907bf60ce6d95e170813a7dfd496
Qiang Bai <chiang.bai@gmail.com>
2022-01-05 15:08:01 +0800

    add more for zsh, spacemacs gdb

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -179,7 +179,7 @@ values."
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("Source Code Pro"
                                :size 18
-                               :weight normal
+                               :weight bold
                                :width normal
                                :powerline-scale 1.1)
    ;; The leader key
