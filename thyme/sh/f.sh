8a55189c7c04f8e906e4dae84101f4c7606e2bc2
Qiang Bai <chiang.bai@gmail.com>
2024-11-12 13:22:28 +0800

    add kinky.sh

---
+++ thyme/sh/f.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# diff_folder folder1 folder2
+diff_folder() {
+    diff <(cd "$1" && find|sort) <(cd "$2" && find|sort) -y |grep -E '>|<'
+}
+
+# show all files for the specfied folder
+lsr() { find "${@:-.}" -print0 | sort -z | xargs -0 ls --color=auto -dlha; }
+
+# test disk speed
+disk_speed() {
+    dd if=/dev/zero of=/var/tmp/infile count=1175000 &>/dev/null
+    for bs in 4k 8k 16k 32k 64k 128k 256k 512k 1M 2M 4M 8M 16M 32M
+    do
+        echo "block size = $bs"
+        dd if=/var/tmp/infile of=/var/tmp/outfile bs=$bs 2>&1 | (grep copied |awk -F ',' '{print "time/speed:" $2,$3}')
+        echo ""
+    done
+    rm -rf /var/tmp/infile /var/tmp/outfile
+}
+
+# download and unzip at the same time
+tar_tee() {
+    wget -O- $1 |tee download.tar.gz|tar xzv
+}
+
+# watch_console /dev/vcs1
+watch_console() {
+	watch -d -n0.5 sudo fold -w 160 $1|tr -cd '\11\12\15\40-\176'|awk 'NE'
+}
+
+ng() {
+	sudo netstat -antp|grep 'listen'|paste -d " " - <(sudo netstat -antp|grep 'listen'|awk '{print $7}'|cut -d'/' -f1|xargs -I {} ps --no-headers -p {} -o cmd)
+}
+
+net_speed() {
+	sudo tcpdump -i $(ip route get 1.2.3.4 | awk '{print $5}') -w -|pv -bert >/dev/null
+}
+
+user_cpu() {
+	echo "User 				CPU Usage (%)"
+	echo "-------------------------------"
+
+	ps -eo user,%cpu,comm,maj_flt,rss |awk '
+		NR > 1 {
+			cpu[$1] += $2
+		}
+		END {
+			for (user in cpu) {
+				printf("%-16s %.2f\n", user, cpu[user])
+			}
+		}
+
+	'| sort -k2 -rn
+}
+
+quick_delete() {
+	mkdir -p /tmp/delete && sudo rsync --delete-before -av /tmp/delete/ "$1"
+}
+
+encrypt_sh() {
+	# read -p "Script: " S 			## read only for bash, as vared for zsh
+	# S=$(bash -c 'read -p "Script: "') && C=$S.crypt H='eval "$((dd if=$0 bs=1 skip=//|gpg -d)2>/dev/null)"; exit; ' && gpg -c<$S|cat > $C <(echo $H|sed s://:$(echo "$H"|wc -c):) - <(chmod +x $C)
+	vared -p 'Script: ' -c S && C=$S.crypt H='eval "$((dd if=$0 bs=1 skip=//|gpg -d)2>/dev/null)"; exit; ' && gpg -c<$S|cat > $C <(echo $H|sed s://:$(echo "$H"|wc -c):) - <(chmod +x $C)
+}
+
+
+ssh_login() {
+	passwd='123456'
+	
+	expect <<-EOF
+	
+	set time 30
+	spawn ssh "$1" df -Th
+	expect {
+	"*yes/no" { send "yes\r"; exp_continue }
+	"*password:" { send "$passwd\r" }
+	}
+	expect eof
+	EOF
+}
+
--- thyme/sh/kinky.sh
+++ thyme/sh/kinky.sh
@@ -1,37 +1,56 @@
 #!/bin/bash
 # #!/usr/bin/env bash
 
-set -e				# exit if error
-#set +e     		# continue if error
-set -u				# exit if undefined var
+set -Eeuo pipefail
+
+#set -e				# exit if error
+#set +e     			# continue if error
+#set -u				# exit if undefined var
 #set -n				# check syntax but not running
 
-#set -x 			# print debug
-#set -o xtrace 		#print debuf
+[[ "${TRACE:-0}" = 1 ]] && set -o xtrace
+#: ${TRACE:=0}
+#[[ "$(TRACE)" = 1 ]] && set -o xtrace
+
+# Script trace mode
+#if [[ -z ${MY_VAR+x} ]]; then
+#if [ "${DEBUG_MODE}" == "true" ]; then
+#    #set -x 			# print debug
+#fi
+#[ "${TRACE}" == 0 ] && {
+#	echo "hello"
+#	echo "world"
+#}
+
+#if [ $UID -gt 199 ] && [ “id -gn” = “id -un” ]; then
+
+
 #set -var			# print param
+#set -o pipefail		# exit if left part of pipe failed(not rightmost)
 
-DOTFILES_ROOT="https://github.com/qbai/dotfiles/thyme/sh/kinky.sh"
+#set -E                         #trig ERR trap for child shell
+#trap 'echo "An error occurred."' ERR
 
+
+
+DOT_FILES="https://github.com/qbai/dotfiles.git"
 
 usage() {
     cat 1>&2 <<EOF
-kinky 1.0.0 (408ed84 2017-02-11)
+kinky 1.0.0 (2024-11-04)
 The installer for kinky
 
 USAGE:
     kinky [FLAGS] [OPTIONS]
 
 FLAGS:
-    -v, --verbose           Enable verbose output
-    -y                      Disable confirmation prompt.
-        --no-modify-path    Don't configure the PATH environment variable
-    -h, --help              Prints help information
-    -V, --version           Prints version information
+    -r --remote             remotely install the dev environment
+    -b --backup             backup the whole system
+    -m --restore            restore and migrate the whole system into the new machine
+    -t, --test              Prints test function
 
 OPTIONS:
-        --default-host <default-host>              Choose a default host triple
-        --default-toolchain <default-toolchain>    Choose a default toolchain to install
-        --default-toolchain none                   Do not install any toolchains
+       
 EOF
 }
 
@@ -40,10 +59,26 @@ err() {
     exit 1
 }
 
+install_cmd() {
+	# or /etc/os-release
+	if grep -qi "debian" /etc/issue; then
+	    sudo apt-get -y install "$1"
+	elif grep -qi "ubuntu" /etc/issue; then
+	    :
+	elif grep -qi "arch" /etc/issue; then
+	    sudo pacman -Sy --noconfirm --needed "$1"
+	elif grep -qi "centos" /etc/issue; then
+	    :
+	elif grep -qi "red hat" /etc/issue || grep -qi "redhat" /etc/issue; then
+	    :
+	else
+	    echo "Unknown Linux distribution"
+	fi
+}
+
 need_cmd() {
-    if ! check_cmd "$1"
-    then err "need '$1' (command not found)"
-    fi
+    check_cmd "$1" || install_cmd "$1"
+    [ $? != 0 ] && err "need '$1' (command not found)"
 }
 
 check_cmd() {
@@ -51,13 +86,26 @@ check_cmd() {
     return $?
 }
 
+
+need_ok() {
+    if [ $? != 0 ]; then err "$1"; fi
+}
+
+# Run a command that should never fail. If the command fails execution
+# will immediately terminate with an error showing the failing
+# command.
+ensure() {
+    "$@"
+    need_ok "command failed: $*"
+}
+
 # This wraps curl or wget. Try curl first, if not installed,
 # use wget instead.
 downloader() {
-    if check_cmd curl
-    then _dld=curl
-    elif check_cmd wget
+    if check_cmd wget
     then _dld=wget
+    elif check_cmd curl
+    then _dld=curl
     else _dld='curl or wget' # to be used in error message of need_cmd
     fi
 
@@ -73,31 +121,110 @@ downloader() {
 
 
 show_file() {
-	cat file.txt | (while read line;do echo $line;done)
+	cat "$1" | (while read line;do echo $line;done)
 }
 
+# block comment	
+: '
+delete_remote() {
+	rm -rf .spacemacs.d scripts .config/micro .imwheelrc .gitconfig .gdbinit .Xmodmap .zprofile .xinitrc .inputrc .bash_profile .bashrc .zshrc .spacemacs.d.org .imwheelrc.org .gitconfig.org .gdbinit.org .Xmodmap.org .zprofile.org .xinitrc.org .inputrc.org .bash_profile.org .bashrc.org .zshrc.org dotfiles/ .emacs.d/ .emacs.d.org/
+}
+'
+
+link_this() {
+	
+	[ -f "./$1" ] && [ ! -L "./$1" ] && mv ./"$1" ./"$1".org 
+	[ -f "./$1" ] && rm -rf ./$1
+	ln -s $(pwd)/dotfiles/"$2" $(pwd)/"$1"
+}
+
+install_remote() {
+	downloader --check
+	#ensure downloader "$DOT_FILES" "/tmp/kinky.sh"
+	need_cmd git
+	need_cmd zsh
+	need_cmd emacs
+	need_cmd micro
+
+	cd ~/Downloads
+	[ -d "./dotfiles.org" ] && rm -rf ./dotfiles.org
+	[ -d "./dotfiles" ] && mv ./dotfiles ./dotfiles.org 		
+	git clone "$DOT_FILES" 
+
+	link_this .bashrc zsh/.bashrc.linux
+	link_this .bash_profile zsh/.bash_profile
+	link_this .inputrc zsh/.inputrc
+	link_this .xinitrc zsh/.xinitrc
+	link_this .zprofile zsh/.zprofile
+	link_this .Xmodmap Xresources/.Xmodmap
+	link_this .gdbinit gdb/.gdbinit
+	link_this .gitconfig git/.gitconfig
+	link_this .imwheelrc mouse/.imwheelrc
+
+	[ ! -d "./.config" ] && mkdir -p ./.config
+	[ -d "./.config/micro" ] && [ ! -L "./.config/micro" ] && mv ./.config/micro ./.config/micro.org
+	[ -d "./.config/micro" ] && rm -rf ./.config/micro
+	ln -s $(pwd)/dotfiles/micro ./.config/micro
+
+	[ -d "./scripts" ] && [ ! -L "./scripts" ] && mv ./scripts ./scripts.org
+	[ -d "./scripts" ] && rm -rf ./scripts	
+
+	ln -s $(pwd)/dotfiles/polybar/scripts $(pwd)/scripts
+
+	[ -d "./.spacemacs.d" ] && [ ! -L "./.spacemacs.d" ] && mv ./.spacemacs.d ./.spacemacs.d.org
+	[ -d "./.spacemacs.d" ] && rm -rf ./.spacemacs.d
+	ln -s $(pwd)/dotfiles/spacemacs $(pwd)/.spacemacs.d
+
+	[ -d "./.emacs.d.org" ] && rm -rf ./.emacs.d.org
+	[ -d "./.emacs.d" ] && mv ./.emacs.d ./.emacs.d.org
+	mkdir ./.emacs.d; git clone https://github.com/syl20bnr/spacemacs.git ./.emacs.d
+
+	[[ -z "${ZSH_VERSION:-""}" ]] || chsh -s $(which zsh)	
+	[ -d "./.oh-my-zsh.org" ] && rm -rf ./.oh-my-zsh.org
+	[ -d "./.oh-my-zsh" ] && mv ./.oh-my-zsh ./.oh-my-zsh.org
+	sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+	# sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	# wget -qO- "https://github.com/qbai/dotfiles/blob/master/thyme/sh/kinky.sh" | bash -s -- arg1 arg2
+	link_this .zshrc zsh/.zshrc.linux
+	
+# :<<!
+# ...
+# !
+}
+
+backup_system() {
+	:
+}
+
+restore_system() {
+	:
+}
 
 main() {
 
-	# handle params
-	for arg in "$@"; do
+    # handle params
+    for arg in "$@"; do
         case "$arg" in
             -h|--help)
                 usage
                 exit 0
                 ;;
-            -y)
-                show_file
+            -r)
+                install_remote
                 ;;
-            -d)
-            	downloader
-            	;;	
+            -b)
+            	backup_system
+            	;;
+            -m)
+            	restore_system
+            	;;
+            -t)
+            	show_file $2
+            	;;
             *)
                 ;;
         esac
     done
-
-
     
 }
 
