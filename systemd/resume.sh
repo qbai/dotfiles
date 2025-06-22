3500d850c9ee7763385823867bdfb6495f3fd82a
Qiang Bai <chiang.bai@gmail.com>
2025-06-22 16:54:28 +0800

    change for exchange super_l & alt_l

--- systemd/resume.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#reset keymap while wakeup
-export DISPLAY=:0
-echo "hello"
-sudo /usr/bin/xmodmap /home/mav/.Xmodmap
---
+++ systemd/startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# waiting x11 is ready
+#for i in {1..100}; do
+#  if xdpyinfo >/dev/null 2>&1; then
+#    break
+#  fi
+#  sleep 1
+#done
+#while [ -z "$(xrandr --listmonitors 2>/dev/null)" ]; do sleep 1; done
+
+#export DISPLAY=:0
+#export XAUTHORITY=/home/mav/.Xauthority
+
+#DISPLAY=:0 XAUTHORITY=/home/mav/.Xauthority setxkbmap -layout us
+sleep 13
+setxkbmap -layout us
+#xmodmap /home/mav/.Xmodmap
